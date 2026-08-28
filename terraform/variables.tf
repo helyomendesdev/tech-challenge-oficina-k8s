e90e1cffa1930851a10ec4bbdd3584b8e515c6bb
sophiasussa <sophiasussa@outlook.com>
2026-08-28 17:10:08 -0300

    feat: adiciona configuração inicial do EKS

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,3 +8,8 @@ variable "vpc_link_security_group_id" {
   description = "ID do Security Group utilizado pelo VPC Link."
   type        = string
 }
+
+variable "eks_cluster_role_arn" {
+  description = "ARN da IAM Role utilizada pelo cluster EKS."
+  type        = string
+}
