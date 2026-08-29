220d0a60cd0c236189106e73c6b9eebc506762f4
sophiasussa <sophiasussa@outlook.com>
2026-08-29 15:02:03 -0300

    infra: adiciona outputs do ALB e seus recursos

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -28,3 +28,18 @@ output "alb_listener_arn" {
   description = "ARN do listener HTTP do ALB interno."
   value       = aws_lb_listener.http.arn
 }
+
+output "alb_arn" {
+  description = "ARN do ALB interno."
+  value       = aws_lb.internal.arn
+}
+
+output "alb_dns_name" {
+  description = "DNS do ALB interno."
+  value       = aws_lb.internal.dns_name
+}
+
+output "alb_target_group_arn" {
+  description = "ARN do Target Group do EKS."
+  value       = aws_lb_target_group.eks.arn
+}
