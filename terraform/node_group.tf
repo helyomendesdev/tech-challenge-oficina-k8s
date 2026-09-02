309de8a84b4852cb16490756a2081f17802611f1
sophiasussa <sophiasussa@outlook.com>
2026-09-02 14:49:28 -0300

    chore: aumenta instância dos nodes para t3.small

--- terraform/node_group.tf
+++ terraform/node_group.tf
@@ -10,7 +10,7 @@ resource "aws_eks_node_group" "main" {
     aws_subnet.private_b.id
   ]
 
-  instance_types = ["t3.micro"]
+  instance_types = ["t3.small"]
 
   scaling_config {
     desired_size = 2
